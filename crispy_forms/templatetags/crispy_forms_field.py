f6a449f1cb8bf4787dfd63dc192d589377f3b59a
brianjarita <brianjarita@users.noreply.github.com>
2023-10-13 00:27:13 -0700

    add bootstrap4 support

--- crispy_forms/templatetags/crispy_forms_field.py
+++ crispy_forms/templatetags/crispy_forms_field.py
@@ -106,7 +106,15 @@ class CrispyFieldNode(template.Node):
                     css_class += " %s" % class_name
             else:
                 css_class = class_name
-
+                
+            if template_pack == "bootstrap4" and not is_multivalue(field):
+                if not is_checkbox(field):
+                    css_class += " form-control"
+                    if is_file(field):
+                        css_class += "-file"
+                if field.errors:
+                    css_class += " is-invalid"
+                    
             widget.attrs["class"] = css_class
 
             for attribute_name, attribute in attr.items():
